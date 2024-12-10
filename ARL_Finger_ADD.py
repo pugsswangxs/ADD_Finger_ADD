060104971beda625d7224292880a51cfb29f5ecd
小王 <91642824+pugsswangxs@users.noreply.github.com>
2024-12-10 10:04:16 +0800

    Update ARL_Finger_ADD.py

--- ARL_Finger_ADD.py
+++ ARL_Finger_ADD.py
@@ -2,21 +2,22 @@
 # -*- coding:utf-8 -*-
 """
 -------------------------------------------------
-Author:       loecho
-Datetime:     2021-07-23 12:47
-ProjectName:  getFinger.py
-Blog:         https://loecho.me
-Email:        loecho@foxmail.com
+Author:       pugss
+Datetime:     2024-12-10 12:47
+ProjectName:  Finger.py
+Blog:         https://www.testerwangxinsheng.top/
+Email:        1270777250@qq.com
 -------------------------------------------------
 """
 import os
-import sys
-import json
-import requests
 import concurrent.futures
 
-requests.packages.urllib3.disable_warnings()
+import json
+import sys
+import requests
+from urllib.parse import urlparse
 
+requests.packages.urllib3.disable_warnings()
 '''
 -----ARL支持字段：-------
 body = " "
@@ -24,9 +25,80 @@ title = ""
 header = ""
 icon_hash = ""
 '''
+if 1 < len(sys.argv) < 6:
+    url = sys.argv[1]
+    login_name = sys.argv[2]
+    login_password = sys.argv[3]
+    if len(sys.argv) >= 5:
+        json_paths = sys.argv[4]
+    else:
+        json_paths = r"./json_path"
+else:
+    print('''
+    usage:    python3 ARl-Finger-ADD.py https://192.168.1.1:5003/ admin password .
+                                                        
+                                                         by  wangxs
+    ''')
+    sys.exit(1)
 
 
-def run_thread(url, token, fingerprint_msg):
+def validate_url(url):
+    """
+    验证 URL 是否有效。
+
+    参数:
+    url (str): 要验证的 URL 字符串。
+
+    返回:
+    bool: 如果 URL 有效则返回 True，否则返回 False。
+    """
+    try:
+        result = urlparse(url)
+        return all([result.scheme, result.netloc])
+    except ValueError:
+        return False
+
+
+def get_session(login_url, login_name, login_password):
+    """
+    获取登录令牌。
+
+    参数:
+    login_url (str): 登录 API 的 URL。
+    login_name (str): 用户名。
+    login_password (str): 密码。
+
+    返回:
+    requests.Session: 包含登录令牌的会话对象。
+
+    异常:
+    ValueError: 如果 URL 无效。
+    requests.exceptions.RequestException: 如果请求失败。
+    """
+    if not validate_url(login_url):
+        raise ValueError("Invalid login URL")
+
+    str_data = {"username": login_name, "password": login_password}
+    login_data = json.dumps(str_data)
+    session = requests.Session()
+    try:
+        data = session.post(f"{login_url}api/user/login", headers={
+            "Accept": "application/json, text/plain, */*",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
+            "Content-Type": "application/json; charset=UTF-8"}, data=login_data, verify=False)
+        if "401" not in data.text:
+            data.raise_for_status()
+            token = data.json()['data']['token']
+            session.headers.update({"Token": f"{token}"})
+            return session
+        else:
+            print("账号或密码错误")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+
+def run_thread(fingerprint_msg,session):
     body = "body=\"{}\""
     title = "title=\"{}\""
     hash = "icon_hash=\"{}\""
@@ -39,7 +111,6 @@ def run_thread(url, token, fingerprint_msg):
                 rule = body.format(rule)
             else:
                 rule = body.format(finger_json['keyword'][0])
-            add_Finger(name, rule, url, token)
 
     elif finger_json['method'] == "keyword" and finger_json['location'] == "title":
         name = finger_json['cms']
@@ -49,7 +120,6 @@ def run_thread(url, token, fingerprint_msg):
                 rule = title.format(rule)
             else:
                 rule = title.format(finger_json['keyword'][0])
-            add_Finger(name, rule, url, token)
     else:
         name = finger_json['cms']
         if len(finger_json['keyword']) > 0:
@@ -57,45 +127,35 @@ def run_thread(url, token, fingerprint_msg):
                 rule = hash.format(rule)
             else:
                 rule = hash.format(finger_json['keyword'][0])
-            add_Finger(name, rule, url, token)
 
-
-def main(url, token, json_path):
-    f = open(json_path, 'r', encoding="utf-8")
-    content = f.read()
-    load_dict = json.loads(content)
-
-    data = load_dict['fingerprint']
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        list(executor.map(run_thread, [url] * len(data), [token] * len(data),
-                          data))
-
-
-def add_Finger(name, rule, url, token):
-    headers = {
-        "Accept": "application/json, text/plain, */*",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
-        "Connection": "close",
-        "Token": "{}".format(token),
-        "Accept-Encoding": "gzip, deflate",
-        "Accept-Language": "zh-CN,zh;q=0.9",
-        "Content-Type": "application/json; charset=UTF-8"
-    }
-    url = "{}/api/fingerprint/".format(url)
+    url_add = f"{url}/api/fingerprint/"
     data = {"name": name, "human_rule": rule}
-    data_json = json.dumps(data)
-
     try:
-        response = requests.post(url, data=data_json, headers=headers, verify=False)
+        response = session.post(url_add, json=data, verify=False)
         if response.status_code == 200:
-            print(''' Add: [\033[32;1m+\033[0m]  {}\n Rsp: [\033[32;1m+\033[0m] {}'''.format(data_json, response.text))
+            print(''' Add: [\033[32;1m+\033[0m]  {}\n Rsp: [\033[32;1m+\033[0m] {}'''.format(data, response.text))
     except Exception as e:
         print(e)
 
 
-def test(name, rule):
-    return print("name: {}, rule: {}".format(name, rule))
+def main(json_path, session):
+    f = open(json_path, 'r', encoding="utf-8")
+    content = f.read()
+    load_dict = json.loads(content)
+    data = load_dict['fingerprint']
+    with concurrent.futures.ThreadPoolExecutor(max_workers=1) as executor:
+        list(executor.map(run_thread, data, [session] * len(data)))
+
+
+def add_Finger(name, rule):
+    url_add = f"{url}/api/fingerprint/"
+    data = {"name": name, "human_rule": rule}
+    try:
+        response = session.post(url_add, json=data, verify=False)
+        if response.status_code == 200:
+            print(''' Add: [\033[32;1m+\033[0m]  {}\n Rsp: [\033[32;1m+\033[0m] {}'''.format(data, response.text))
+    except Exception as e:
+        print(e)
 
 
 def get_json_name_from_path(file_path):
@@ -105,46 +165,10 @@ def get_json_name_from_path(file_path):
 
 if __name__ == '__main__':
     try:
-        if 1 < len(sys.argv) < 6:
-
-            login_url = sys.argv[1]
-            login_name = sys.argv[2]
-            login_password = sys.argv[3]
-            if len(sys.argv) >= 5:
-                json_paths = sys.argv[4]
-            else:
-                json_paths = "."
-
-            # login
-            str_data = {"username": login_name, "password": login_password}
-            login_data = json.dumps(str_data)
-            login_res = requests.post(url="{}api/user/login".format(login_url), headers={
-                "Accept": "application/json, text/plain, */*",
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36",
-                "Content-Type": "application/json; charset=UTF-8"}, data=login_data, verify=False)
-
-            # 判断是否登陆成功：
-            if "401" not in login_res.text:
-
-                # print(type(login_res.text))
-                token = json.loads(login_res.text)['data']['token']
-                print("[+] Login Success!!")
-                json_lists = get_json_name_from_path(json_paths)
-
-                with concurrent.futures.ProcessPoolExecutor(max_workers=2) as executor:
-                    list(executor.map(main, [login_url] * len(json_lists), [token] * len(json_lists),
-                                      json_lists))
-                    executor.shutdown(wait=True)
-
-            else:
-                print("[-] login Failure! ")
-        else:
-            print('''
-    usage:
-        
-        python3 ARl-Finger-ADD.py https://192.168.1.1:5003/ admin password .
-                                                        
-                                                         by  loecho
-            ''')
+        json_lists = get_json_name_from_path(json_paths)
+        session = get_session(url, login_name, login_password)
+        with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
+            list(executor.map(main, json_lists, [session] * len(json_lists)))
+            executor.shutdown(wait=True)
     except Exception as a:
         print(a)
